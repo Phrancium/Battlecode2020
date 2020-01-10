256a6b9525ca7b885fe6c311c96b538ddc8c6c62
Phrancium <tianfrank@gmail.com>
2020-01-10 15:45:24 -0500

    Revert "Miners can mine and refine soup"
    This reverts commit 5b95d2d745fbae687d453a258b81d9571d81257b.

--- src/Frankplayer1/RobotPlayer.java
+++ src/Frankplayer1/RobotPlayer.java
@@ -86,11 +86,6 @@ public strictfp class RobotPlayer {
 
     static void runMiner() throws GameActionException {
         MapLocation curr = rc.getLocation();
-        if(canMine() && rc.getSoupCarrying() < 98) {
-            mineSoup();
-        }else if(rc.getSoupCarrying() > 98){
-            goHome(curr);
-        }
         if(!(destination == null)){
             if(destination.x == curr.x && destination.y == curr.y){
                 destination = null;
@@ -100,39 +95,6 @@ public strictfp class RobotPlayer {
         }else {
             findSoup(curr);
         }
-    }
-
-    /**robot mines soup **/
-    static boolean canMine() throws GameActionException{
-        for (Direction l : directions){
-            if (rc.canMineSoup(l)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static void mineSoup() throws GameActionException{
-        for (Direction l : directions) {
-            if (rc.canMineSoup(l)) {
-                rc.mineSoup(l);
-            }
-        }
-    }
-    /**
-     * Scouting method run at beginning to find soup
-     *
-     * @throws GameActionException
-     */
-    static void goHome(MapLocation m) throws GameActionException {
-        Direction d = randomDirection();
-        for (Direction l : directions) {
-            if (rc.canDepositSoup(l)) {
-                System.out.println("I deposited soup");
-                rc.depositSoup(l, rc.getSoupCarrying());
-            }
-        }
-        tryMove(d);
     }
 
     static void runRefinery() throws GameActionException {
@@ -262,7 +224,7 @@ public strictfp class RobotPlayer {
                 MapLocation check = new MapLocation(selfX + x, selfY + y);
                 if (rc.canSenseLocation(check)){
                     if(rc.senseSoup(check) > 0){
-                        moveTo(m, new MapLocation(check.x-1, check.y));
+                        moveTo(m, check);
                         //upload location of soup to blockchain?
                     }
                 }
