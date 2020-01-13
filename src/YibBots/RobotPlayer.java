36a6b2146027a140bd189c01ba3a4453563fab04
daniely3 <daniely3@illinois.edu>
2020-01-13 17:27:54 -0500

    net gun

--- src/YibBots/RobotPlayer.java
+++ src/YibBots/RobotPlayer.java
@@ -302,8 +302,14 @@ public strictfp class RobotPlayer {
     }
 
     static void runNetGun() throws GameActionException {
-
+        RobotInfo[] r = rc.senseNearbyRobots();
+        for(RobotInfo s : r){
+            if(s.getTeam() != rc.getTeam() && rc.canShootUnit(s.getID())){
+                rc.shootUnit(s.getID());
+            }
+        }
     }
+
 
     /**
      * Returns a random Direction.
