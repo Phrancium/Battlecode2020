6b93c0ef3bb9a99b8ab63a8a6035bf2a2c2d509c
Azyrez <57201161+Azyrez@users.noreply.github.com>
2020-01-13 00:22:35 -0500

    Update RobotPlayer.java

--- src/YibBots/RobotPlayer.java
+++ src/YibBots/RobotPlayer.java
@@ -218,13 +218,8 @@ public strictfp class RobotPlayer {
             findEnemyHQ(rc.getLocation());
         MapLocation current = rc.getLocation();
         //alternate moving with picking up dirt
-        if(current.distanceSquaredTo(HQ)>1) {
-            if(turnCount % 2 == 0) {
-                if (rc.getDirtCarrying() < RobotType.LANDSCAPER.dirtLimit && rc.canDigDirt(Direction.CENTER))
-                    rc.digDirt(Direction.CENTER);
-            }
-            else
-                moveTo(HQ);
+        if(current.distanceSquaredTo(HQ)>1.8) {
+            moveTo(HQ);
         }
         //if HQ is within range
         else if(rc.getDirtCarrying() > 0) {
