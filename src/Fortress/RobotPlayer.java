dbb695240b20ff147af4a79c7b7ad58dd60f532e
wwll7 <7bobperson7@gmail.com>
2020-01-20 16:48:35 -0500

    hover drone 100 units

--- src/Fortress/RobotPlayer.java
+++ src/Fortress/RobotPlayer.java
@@ -1604,7 +1604,7 @@ public strictfp class RobotPlayer {
     }
     static void moveToDroneHover(MapLocation dest) throws GameActionException{
 
-        if (!dest.equals(prevdest)){
+        if (!dest.equals(prevdest) || (dest==HQ && dest.distanceSquaredTo(rc.getLocation())>100)){
             prevdest=dest;
             prevLocations.clear();
         }
